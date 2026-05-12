255530c5dd660a6df49397b0668f8a6305bfddcc
Ubuntu <ubuntu@ip-172-31-36-12.ap-south-1.compute.internal>
2026-05-12 21:00:13 +0000

    Final Selenium Fix

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 
-driver = webdriver.Chrome(options=options)
+service = Service('/usr/bin/chromedriver')
+
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("file:///var/snap/jenkins/5022/workspace/devops-pipeline4/index.html")
 
@@ -15,6 +19,6 @@ text = driver.find_element(By.TAG_NAME, "h1").text
 
 assert "Success" in text
 
-print("Test Passed")
+print("Test Passed Successfully")
 
 driver.quit()
